8fc1f6e213c352a0ce0e3a74b71501a6f7fa0b02
Ngnawen Samuel <ngnawens@gmail.com>
2023-12-23 14:42:10 -0500

    :loud_sound: Add
    add log info

--- src/main/java/com/belrose/springbootk8s/controller/MessageController.java
+++ src/main/java/com/belrose/springbootk8s/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.belrose.springbootk8s.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
 public class MessageController {
 
     @GetMapping("/message")
     public ResponseEntity<String> displayMessage(){
+        log.info("log for checking deployment to docker hub...");
         return ResponseEntity.ok("Congratulation you successfully deployed your application to kubernetes !!");
     }
 }
